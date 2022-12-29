553529e9fb44c42b8be2f77a18e7f9f84ad96242
yogita_misal <yogitamisal12@gmail.com>
2022-12-29 18:22:50 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -2,3 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+const app = express();
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+const CONNECTION_URL ='mongodb+srv://yogi-25:Seebeyond@cluster0.fs8vy.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL,{useUrlParser:true,useUnifiedTopology: true})
+.then(())
