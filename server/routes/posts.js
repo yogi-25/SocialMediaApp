d35230d4020d00aa4a81dfc96269046865b95936
yogita_misal <yogitamisal12@gmail.com>
2022-12-30 21:35:18 +0530

    Added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,5 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js';
 const router = express.Router();
-router.get('/',(req,res)=> {
-    res.send('THIS WORKS!');
-});
+router.get('/',getPosts);
 export default router;
