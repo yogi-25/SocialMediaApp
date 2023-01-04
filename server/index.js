03c2fd4180a90f87c6a03db0b9eb67438ce8a2b5
yogita_misal <yogitamisal12@gmail.com>
2023-01-04 23:04:59 +0530

    enter

--- server/index.js
+++ server/index.js
@@ -11,4 +11,4 @@ app.use(cors());
 const CONNECTION_URL ='mongodb+srv://yogi-25:Seebeyond@cluster0.fs8vy.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology: true}).then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`))).catch((error)=>console.log(error.message));
-mongoose.set('useFindAndModify',false);
+console.log("helllo  welcome to my world!!!")
